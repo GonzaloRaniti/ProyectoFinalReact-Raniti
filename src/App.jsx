0aca9f67137f298cae631b42ae4891d27dbddf9d
GonzaloRaniti <Gonzalog.raniti@gmail.com>
2023-08-23 19:06:51 -0300

    Agregando contenido

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from './components/navbar';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
     return (
         <div className="App">
             <Navbar />
-            
+            <ItemListContainer greeting={'Bienvenidos a mi E-Commerce'} />
         </div>
     );
 }
